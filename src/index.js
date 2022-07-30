9aa80dbd274ab59c1be7b06d96ba2fd9f2ea0e6f
pavelgre4a <pavelgre4a@gmail.com>
2022-07-30 15:50:21 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let output = '';
+    n = n.toString();
+    
+
+    for (let i = (n.length - 1); i >= 0 && i <= n.length; i--) {
+        if (n.charAt(i) == '-') {
+
+        } else {
+        output = output + n.charAt(i)
+
+        }
+    }
+    return output
 }
